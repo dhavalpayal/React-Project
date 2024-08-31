8a5383bd6d11f07ef1a5b8a3223dfc1d4b330e90
payal patel <jaydipa94patel@gmail.com>
2024-08-31 23:35:33 +0530

    my react project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import EmployeeComponent from './components/EmployeeComponent'
+
+function App() {
+
+  return (
+    <>
+       <BrowserRouter>
+                <HeaderComponent/>
+                <Routes>
+                    //http://localhost:3000
+                    <Route path="/" element={<ListEmployeeComponent />} />
+                    //http://localhost:3000/employee
+                    <Route path="/employee" element={<ListEmployeeComponent />} />
+                    //http://localhost:3000/add-employee
+                    <Route path="/add-employee" element={<EmployeeComponent />} />
+                    
+                </Routes>
+                <FooterComponent/>
+            </BrowserRouter>
+    </>
+  )
+}
+
+export default App
